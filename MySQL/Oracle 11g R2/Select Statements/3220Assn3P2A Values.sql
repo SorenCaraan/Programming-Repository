9b8e7f293fa3e169a65719ff77f2d9b42d1c7907
Soren Caraan <71570754+SorenCaraan@users.noreply.github.com>
2021-05-06 22:33:36 -0400

    Create 3220Assn3P2A Values.sql

---
+++ MySQL/Oracle 11g R2/Select Statements/3220Assn3P2A Values.sql
@@ -0,0 +1,20 @@
+set echo on
+
+INSERT INTO RestaurantN(RestID,Rname,Cuisine)
+VALUES
+    ('r1', 'Helios', 'International');
+INSERT INTO RestaurantN(RestID,Rname,Cuisine)
+VALUES    
+    ('r2', 'El_Gaucho', 'Argentine');
+INSERT INTO RestaurantN(RestID,Rname,Cuisine)
+VALUES
+    ('r3', 'La_Boheme', 'French');
+INSERT INTO RestaurantN(RestID,Rname,Cuisine)
+VALUES
+    ('r4', 'Tentazione', 'Italian');
+INSERT INTO RestaurantN(RestID,Rname,Cuisine)
+VALUES
+    ('r5', 'Capricho', 'International');
+INSERT INTO RestaurantN(RestID,Rname,Cuisine)
+VALUES
+    ('r6', 'Bluewater', 'French');
