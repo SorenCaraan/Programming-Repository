53d8f98300f59c87d77c597250a232e3d8bb0396
Soren Caraan <sorencaraan@outlook.com>
2022-07-23 22:01:14 -0400

    Edited to use "numpy" for mean and mode

--- Python/meanMedianModePy.py
+++ Python/meanMedianModePy.py
@@ -1,10 +1,11 @@
+import numpy
 import statistics
 
 speed = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
 slow = [99, 86, 87, 88, 86, 103, 87, 94, 78, 77, 85, 86]
 
-x = statistics.mean(speed)
-y = statistics.median(slow)
+x = numpy.mean(speed)
+y = numpy.median(slow)
 z = statistics.mode(slow)
 
 print(x)
