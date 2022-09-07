00e509bafc24ad64c8e9c666dc3e0f2a7e081636
Soren Caraan <sorencaraan@outlook.com>
2022-09-07 00:13:09 -0400

    Biography HTML Project Added to Archive

---
+++ HTML/BiographyProject/script.js
@@ -0,0 +1,8 @@
+// @author Soren Caraan
+
+function submitAlert(){
+    alert("Your Comments have been submitted, Thank You!");
+    document.getElementById("textFN").value="";
+    document.getElementById("textLN").value="";
+    document.getElementById("subject").value="";
+}
