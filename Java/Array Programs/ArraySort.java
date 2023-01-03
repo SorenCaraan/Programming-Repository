73870f054b619323b6c615fa00eb5e7830aac819
Soren Caraan <sorencaraan@outlook.com>
2023-01-02 20:59:46 -0500

    Fixed ArraySort.java from "Sorting Algorithm Programs Repository"

--- Java/Array Programs/ArraySort.java
+++ Java/Array Programs/ArraySort.java
@@ -3,47 +3,51 @@ public class ArraySort
 {
 	public static void main(String[] args)
 	{
-	 Scanner scan = new Scanner(System.in);
-	 int[] array = new int[5];
-	
-	 for (int i=0; i<array.length; i++){
-		 array[i] = scan.nextInt();
-	 }
-	 for (int j=array.length-1; j>=0; j--)
-	 {
-		 System.out.println(array[j]);
-	 }
-		 
-		 for (int i = 0; i < array.length; i++)
-		{
-			System.out.print(array[i] + " - ");
-			int larger = 0;
-			int smaller = 0;
-			int equal = 0;
 
-			for (int j = 0; j < array.length; j++)
+	 try (Scanner scan = new Scanner(System.in)) {
+
+		 int[] array = new int[5];
+
+		 System.out.println("Please enter 5 integers: ");
+		 for (int i=0; i<array.length; i++){
+			 array[i] = scan.nextInt();
+		 }
+		 for (int j=array.length-1; j>=0; j--)
+		 {
+			 System.out.println(array[j]);
+		 }
+			 
+			 for (int i = 0; i < array.length; i++)
 			{
-				if (i!=j)
+				System.out.print(array[i] + " - ");
+				int larger = 0;
+				int smaller = 0;
+				int equal = 0;
+
+				for (int j = 0; j < array.length; j++)
 				{
-					if (array[j] > array[i])
+					if (i!=j)
 					{
-						larger++;
-					}
-					else if (array[j] < array[i])
-					{
-						smaller++;
-					}
-					else
-					{
-						equal++;
+						if (array[j] > array[i])
+						{
+							larger++;
+						}
+						else if (array[j] < array[i])
+						{
+							smaller++;
+						}
+						else
+						{
+							equal++;
+						}
 					}
 				}
-			}
 
-			System.out.print(larger + " larger, ");
-			System.out.print(smaller + " smaller, ");
-			System.out.println(equal + " equal");
-		}
+				System.out.print(larger + " larger, ");
+				System.out.print(smaller + " smaller, ");
+				System.out.println(equal + " equal");
+			}
+	}
 	 
 	}
 }
