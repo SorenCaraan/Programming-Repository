2ca158317182dcd406aaedf7768e171677c4a6aa
Soren Caraan <sorencaraan@outlook.com>
2023-01-21 18:44:52 -0500

    Added JavaScript

---
+++ JavaScript/JS Back End/Node.js/addNums.js
@@ -0,0 +1,5 @@
+// Adding Numbers 
+var sum = 10 + 0;
+sum = 10 + 10;
+
+console.log(sum);
---
+++ JavaScript/JS Back End/Node.js/data_type.js
@@ -0,0 +1,11 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Beau"
+
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14 // Cannot be changed
